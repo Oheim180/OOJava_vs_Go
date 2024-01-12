c3693861505c658d0937756ec5cfe6177f0c5ee0
Oheim180 <73171617+Oheim180@users.noreply.github.com>
2024-01-12 09:03:32 +0100

    Add files via upload

---
+++ JavaVsGo/src/beispiel2/Buerger.java
@@ -0,0 +1,8 @@
+package beispiel2;
+
+public class Buerger extends Person{
+	int kundenNr;
+	public Buerger(String name) {
+		super(name);
+	}
+}
--- JavaVsGo/src/beispiel2/Main.java
+++ JavaVsGo/src/beispiel2/Main.java
@@ -5,9 +5,11 @@ public class Main {
 		Person p = new Person("Max");
         Mitarbeiter m = new Mitarbeiter("Bernd");
         Kunde k = new Kunde("Herbert");
-
+        Buerger b = new Buerger("Manfred");
+        
         System.out.println(p.sprechen());
         System.out.println(m.sprechen());
         System.out.println(k.sprechen());
+        System.out.println(b.sprechen());
     }
 }
